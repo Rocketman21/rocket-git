7747df4255e9df8fe9b8f2a8ac914a379d492719
Stanislav Suzdalev <stasuz@mail.ru>
2024-05-03 11:00:22 +0300

    BIG UPDATE: - gci without -m now - nvim by default everywhere - new sourceal alias - update dev alias - pushalupd renamed to publishal - getalupd renamed to updateal - each command now works via git -C, so no cd from your current dir - remove dead code

--- rocket-git.plugin.zsh
+++ rocket-git.plugin.zsh
@@ -1,21 +1,29 @@
-alias gst='git status'
-alias glg='git lg'
-alias gbr='git branch'
-alias gco='git checkout'
-alias gcob='git checkout -b'
-alias gcm='git checkout master'
-alias gpl='git pull'
-alias gpsh='git push'
-alias gpf='git push -f'
-alias gci='git add . && git commit -m'
-alias grb='git rebase'
-alias grm='git rebase master'
-alias gca='git add . && git commit --amend --no-edit'
-alias gcd='~/.oh-my-zsh/plugins/rocket-git/gcd.sh'
+export ROCKET_GIT_DIR="$HOME/.oh-my-zsh/plugins/rocket-git"
+export ROCKET_GIT_PLUGIN="$ROCKET_GIT_DIR/rocket-git.plugin.zsh"
 
-alias edital='nano ~/.oh-my-zsh/plugins/rocket-git/rocket-git.plugin.zsh'
-alias editalc='code ~/.oh-my-zsh/plugins/rocket-git'
-alias printal='cat -n ~/.oh-my-zsh/plugins/rocket-git/rocket-git.plugin.zsh'
-alias lint='npm run lint:err'
-alias getalupd='cd ~/.oh-my-zsh/plugins/rocket-git/ && gpl'
-alias pushalupd='cd ~/.oh-my-zsh/plugins/rocket-git/ && gci "alias update $(date +"%Y-%m-%d %H:%M")" && gpsh'
+alias gst="git status"
+alias glg="git lg"
+alias gbr="git branch"
+
+alias gco="git checkout"
+alias gcob="git checkout -b"
+alias gcm="git checkout master"
+
+alias gpl="git pull"
+alias gpsh="git push"
+alias gpf="git push -f"
+
+alias gci="git add . && git commit"
+alias gca="git add . && git commit --amend --no-edit"
+alias gcd="$ROCKET_GIT_DIR/gcd.sh"
+
+alias grb="git rebase"
+alias grm="git rebase master"
+
+alias lint="npm run lint:err"
+alias dev="./server.sh stage316.s1"
+
+alias edital="nvim $ROCKET_GIT_PLUGIN"
+alias updateal="git -C $ROCKET_GIT_DIR pull && sourceal"
+alias publishal="git -C $ROCKET_GIT_DIR add . && git -C $ROCKET_GIT_DIR commit && git -C $ROCKET_GIT_DIR push"
+alias sourceal="source $ROCKET_GIT_PLUGIN"
