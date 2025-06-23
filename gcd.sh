f8b965782abe89ef4e84a431c30a2bf0228c662a
Суздалев Станислав <suzdalev@kinoplan.tech>
2025-06-23 10:22:16 +0300

    Change gcd to work better with params

--- gcd.sh
+++ gcd.sh
@@ -12,15 +12,17 @@ while getopts 'a:h' opt; do
 			gitadd="git add . &&"
 			;;
 		?|h)
-			echo "Usage: $(basename $0) [-a]"
+			echo "Usage: $(basename $0) [-a --] <date> [additional git flags]"
 			exit 1
 			;;
 	esac
 done
-shift "$(($OPTIND -1))"
-echo "gitadd = $gitadd; 0 = $0; 1 = $1; 2 = $2"
 
-cmd="$gitadd git commit --date=\"$1 $(rnd 10 23):$(rnd 0 5)$(rnd 0 9):$(rnd 0 5)$(rnd 0 9) +0300\" $2";
+shift "$(($OPTIND -1))"
+commit_date="$1"
+shift
+
+cmd="$gitadd git commit --date=\"$commit_date $(rnd 10 23):$(rnd 0 5)$(rnd 0 9):$(rnd 0 5)$(rnd 0 9) +0300\" $@";
 
 echo -e "Executing: \033[1;32m$cmd\033[0m";
 
